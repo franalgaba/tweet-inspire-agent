7ad1461a833dce4dab95cbbf04e11a2378975dc4
Fran Algaba <fran@gizatech.xyz>
2025-12-15 20:10:54 +0100

    Fixed bun build reference

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 import tsConfigPaths from 'vite-tsconfig-paths'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [
@@ -9,6 +13,11 @@ export default defineConfig({
     tsConfigPaths(),
     react(),
   ],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     port: 3000,
     host: true,
